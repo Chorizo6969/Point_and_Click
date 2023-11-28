e82739adf276e776ded028b482220d754987712a
Lucas Pécout <lucas.pecout@e-artsup.net>
2023-11-28 16:41:24 +0100

    texte en mode machine à écrire

---
+++ Assets/Scripts/dialogue.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class dialogue : MonoBehaviour
+{
+    public TMP_Text monTexte;
+    public string texte;
+    public float delay;
+    Coroutine maCoroutine;
+
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            StopCoroutine(maCoroutine);
+            monTexte.maxVisibleCharacters = monTexte.text.Length;
+        }
+    }
+
+    public void OnEnable()
+    {
+        monTexte.text = texte;
+        monTexte.maxVisibleCharacters = 0;
+        maCoroutine = StartCoroutine(texteLettreParLettre());
+        
+    }
+
+    public void OnDisable()
+    {
+        monTexte.text = null;
+    }
+
+    IEnumerator texteLettreParLettre()
+    {
+        for (int i = 0; i <= monTexte.text.Length; i++)
+        {
+            monTexte.maxVisibleCharacters++;
+            yield return new WaitForSeconds(delay);
+        }
+    }
+    
+
+}
