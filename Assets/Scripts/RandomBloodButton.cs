7be3be3f7ad186e9e4cfcc94724db11026407586
Chorizo <mateoledru05@gmail.com>
2023-11-30 11:07:04 +0100

    1er étape pour randomiser le sang

--- Assets/Scripts/RandomBloodButton.cs
+++ Assets/Scripts/RandomBloodButton.cs
@@ -1,16 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+
 
 public class RandomBloodButton : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public TMP_Text randomresult;
+
+    List<string> randomButton = new List<string> { "O+", "O-", "A+", "A-", "B+", "B-", "AB+", "AB-" };
+
+    public void ButtonClick()
+    {
+        Debug.Log("jsdbviosd");
+        PrintBlood();
+    }
+
+    private void PrintBlood()
+    {
+        string randomName = randomButton[Random.Range(0, randomButton.Count)];
+        randomresult.text = randomName;
+    }
+
+
+
+
+
+
+
+
+
+
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
