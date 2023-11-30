b7fea7bb865387dc5c3f959efda135927c107fc5
zwazoman <gloupik.lemaire@gmail.com>
2023-11-30 11:32:30 +0100

    panel chépa et tout

---
+++ Assets/Scripts/lvl bombe/ChancesRestantes.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ChancesRestantes : MonoBehaviour
+{
+    public TMP_Text TextNbChances;
+    private string TextOriginal;
+    public int NbChances = 3;
+    
+    private void Start()
+    {
+        TextOriginal = TextNbChances.text;
+        TextNbChances.text = TextOriginal + NbChances.ToString();
+    }
+    public void ChangeScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void ReduireNbChances()
+    {
+        NbChances -= 1;
+        TextNbChances.text = TextOriginal + " " + NbChances.ToString();
+        if (NbChances == 0)
+        {
+            ChangeScene("GameOver");
+        }
+    }
+    
+}
