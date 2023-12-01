8cca2241be6c503b32360047b59c332a3371acaa
AsaeNeopie <n.noahchery@gmail.com>
2023-12-01 11:19:56 +0100

    Changement cam

--- Assets/Scripts/LumiereAllumer.cs
+++ Assets/Scripts/LumiereAllumer.cs
@@ -9,8 +9,10 @@ public class LumiereAllumer : MonoBehaviour
     public ComputeShader ShaderBg;
     public RenderTexture RT;
     public float TailleDuRond = 1;
-    public float Diviseur = 500;
+    public float multiplicateur = 0.005f;
     public float Puissance = 1;
+
+    public Vector2 PositionCercle2;
     void OnRenderImage(RenderTexture src, RenderTexture dest)
     {
         if (RT == null || RT.height != Screen.height || RT.width != Screen.width) 
@@ -23,10 +25,13 @@ public class LumiereAllumer : MonoBehaviour
         ShaderBg.SetVector("PositionDeLaSouris", Input.mousePosition);
         ShaderBg.SetTexture(0, "Input", src);
         ShaderBg.SetTexture(0, "Result", RT);
+        ShaderBg.SetFloat("ratio", 1080f/(float)Screen.height);
 
         ShaderBg.SetFloat("TailleDuCercle", TailleDuRond);
-        //ShaderBg.SetFloat("Diviseur", Diviseur);
-        //ShaderBg.SetFloat("Puissance", Puissance);
+        ShaderBg.SetFloat("multiplicateur", multiplicateur);
+        ShaderBg.SetFloat("Puissance", Puissance); 
+
+        ShaderBg.SetVector("PositionCercle2", PositionCercle2);
 
         ShaderBg.Dispatch(0, RT.width/8, RT.height/8, 1);
         
