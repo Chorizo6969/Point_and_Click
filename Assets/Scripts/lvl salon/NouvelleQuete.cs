58f3e4986313b21a0e65af23bcddad824408852c
zwazoman <gloupik.lemaire@gmail.com>
2023-12-04 19:39:40 +0100

    légère avancée : Nathan aled

--- Assets/Scripts/lvl salon/NouvelleQuete.cs
+++ Assets/Scripts/lvl salon/NouvelleQuete.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NouvelleQuete : MonoBehaviour
 {
-    public string nomquete;
-    public void Ajouterquete(string nomquete)
+    public void Ajouterquete()
     {
-
+        GameObject quete = new GameObject();
+        quete.transform.position = new Vector2(0, 42);
+        quete.AddComponent<TextMesh>();
     }
 }
