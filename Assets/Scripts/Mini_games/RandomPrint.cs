b345ccf11ba8c289c2d62ebb06a483b327895546
Chorizo <mateoledru05@gmail.com>
2023-12-05 15:35:00 +0100

    Fin des mini-jeux#1

---
+++ Assets/Scripts/Mini_games/RandomPrint.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+//public class RandomPrint : MonoBehaviour
+//{
+   // public TMP_Text randomresult;
+    //public GameObject buttonParent;
+   // public GameObject PanelWrong;
+    //public GameObject PanelGood;
+
+    //List<GameObject> RandomButton = new List<GameObject> { "Print0", "Print1", "Print2", "Print3", "Print4", "Print5", "Print6", "Print7", "Print 8" };
+
+    //public void ButtonClick()
+   // {
+       // PrintPrint();
+    //}
+
+    //private void PrintPrint()
+   // {
+        //string randomName = RandomButton[Random.Range(0, RandomButton.Count)];
+        //randomresult.text = randomName;
+   // }
+
+    //public void PrintButtonClick(GameObject self)
+    //{
+
+            //if (self.GetComponent<TMP_Text>().text == randomresult.text)
+          //  {
+                //PanelGood.SetActive(true);
+           // }
+            //else
+           // {
+                //PanelWrong.SetActive(true);
+            //}
+  //  }
+//}
