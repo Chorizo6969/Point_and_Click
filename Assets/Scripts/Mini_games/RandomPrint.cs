b1170e3ba60e5b02959ae56db934db99cdac2a16
Chorizo <mateoledru05@gmail.com>
2023-12-05 17:09:07 +0100

    Fin des mini-games#2

--- Assets/Scripts/Mini_games/RandomPrint.cs
+++ Assets/Scripts/Mini_games/RandomPrint.cs
@@ -4,36 +4,43 @@ using UnityEngine;
 using TMPro;
 
 
-//public class RandomPrint : MonoBehaviour
-//{
-   // public TMP_Text randomresult;
-    //public GameObject buttonParent;
-   // public GameObject PanelWrong;
-    //public GameObject PanelGood;
+public class RandomPrint : MonoBehaviour
+{
+    public TMP_Text randomresult;
+    public GameObject buttonParent;
+    public GameObject PanelWrong;
+    public GameObject PanelGood;
 
-    //List<GameObject> RandomButton = new List<GameObject> { "Print0", "Print1", "Print2", "Print3", "Print4", "Print5", "Print6", "Print7", "Print 8" };
+    List<string> RandomButton = new List<string> { "Empreinte 1", "Empreinte 2", "Empreinte 3", "Empreinte 4", "Empreinte 5", "Empreinte 6", "Empreinte 7", "Empreinte 8" };
 
-    //public void ButtonClick()
-   // {
-       // PrintPrint();
-    //}
+    public void ButtonClick()
+    {
+     PrintPrint();
+     Debug.Log("B");
+    }
 
-    //private void PrintPrint()
-   // {
-        //string randomName = RandomButton[Random.Range(0, RandomButton.Count)];
-        //randomresult.text = randomName;
-   // }
+    private void PrintPrint()
+    {
+        string randomName = RandomButton[Random.Range(0, RandomButton.Count)];
+        randomresult.text = randomName;
+    }
 
-    //public void PrintButtonClick(GameObject self)
-    //{
+    public void Update(){
+        
+    }
 
-            //if (self.GetComponent<TMP_Text>().text == randomresult.text)
-          //  {
-                //PanelGood.SetActive(true);
-           // }
-            //else
-           // {
-                //PanelWrong.SetActive(true);
-            //}
-  //  }
-//}
+    public void PrintButtonClick(GameObject self)
+    {
+
+          if (self.GetComponent<TMP_Text>().text == randomresult.text)
+          {
+               PanelGood.SetActive(true);
+               Debug.Log("C");
+          }
+            else
+            {
+               PanelWrong.SetActive(true);
+               Debug.Log("D");
+            }
+    }
+}
